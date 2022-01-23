f9bb4ed4393d2c71d48793e50b3175485d1b99a2
Solstice245 <sc2solstice245@gmail.com>
2022-01-22 19:03:32 -0700

    cleanup

--- sc_import.py
+++ sc_import.py
@@ -1,24 +1,3 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-# ##### BEGIN GPL LICENSE BLOCK #####
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-#
-# ##### END GPL LICENSE BLOCK #####
-
 import bpy
 import bmesh
 from mathutils import Matrix, Vector, Quaternion
@@ -209,8 +188,7 @@ def scm_mesh_object(scm, arm_ob, dirname, filename, options, bp=None, lod=0):
         if lod > 0: ob.vertex_groups.new(name=sc_bone['name'])
         else: ob.vertex_groups.new(name=sc_bone['name'])
 
-    if lod > 0:
-        ob.display_type = 'WIRE'
+    if lod > 0: ob.display_type = 'WIRE'
 
     scm_mesh(scm, ob, dirname, filename, options, bp, lod)
 
